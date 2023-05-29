a4be6222071ab0fc1ab8630219b46bc49b33e20b
Cooper Werner <cooperwerner824@gmail.com>
2023-05-29 12:03:52 -0400

    initial commit

---
+++ app.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="w-full h-screen flex flex-col items-center justify-around bg-blue-950">
+    <h1 class="h-16 text-4xl text-white">WebGPU Conway Game of Life</h1>
+    <canvas class="w-[512px] aspect-square" id="gameDisplay"></canvas>
+  </div>
+</template>
